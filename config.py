f3e467ce651d28745021235f197a0c75a1a50e60
rex <hazielgtz@hotmail.com>
2026-02-16 18:51:56 -0600

    Insertar 16/02/2026

--- config.py
+++ config.py
@@ -8,5 +8,5 @@ class Config(object):
 
 class DevelopmentConfig(Config):
     DEBUG = True
-    SQLALCHEMY_DATABASE_URI = '´mysql+pymysql://root:root@localhost/bdidgs801'
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:root@localhost/bdidgs801'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
